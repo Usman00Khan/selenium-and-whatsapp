fd1d0a74b3b3c6092ab55a1ae329970de7426e70
usman00khan <uk80718@gmail.com>
2020-05-10 20:21:01 +0530

    modified

--- whatsapp.py
+++ whatsapp.py
@@ -3,9 +3,10 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get("https://web.whatsapp.com/")
-name = "name"
-msg = "message"
-count = 1000 #no of time to send the message
+name = "Image"
+msg = """
+test
+"""
 while True:
     try:
         user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
@@ -16,8 +17,8 @@ while True:
 
 print("\n\n\n ",user,'\n\n\n')
 driver.implicitly_wait(3)
-msg_box = driver.find_element_by_class_name('_3u328.copyable-text.selectable-text')
-for i in range(count):
+msg_box = driver.find_elements_by_class_name('_2S1VP.copyable-text.selectable-text')[1]
+for i in range(1):
     print(i)
-    msg_box.send_keys(str(i) +'. ' + msg)
+    msg_box.send_keys(msg)
     msg_box.send_keys(Keys.RETURN)
