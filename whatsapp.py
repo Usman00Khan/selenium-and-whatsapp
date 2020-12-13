f136c331bd7526964d316a40ecbad01dfb4deee3
Usman Khan <uk80718@gmail.com>
2020-12-13 22:54:47 +0530

    updated for chome ver 87

--- whatsapp.py
+++ whatsapp.py
@@ -1,23 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Chrome()
-driver.get("https://web.whatsapp.com/")
+options = webdriver.ChromeOptions()
+options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+chrome_driver_binary = "./chromedriver"
+driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+driver.get("https://web.whatsapp.com/") 
 name = "Image"
 msg = """
 test
 """
 while True:
     try:
-        user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+        user = driver.find_element_by_xpath(
+            '//span[@title = "{}"]'.format(name))
         user.click()
         break
     except:
         pass
 
-print("\n\n\n ",user,'\n\n\n')
+print("\n\n\n ", user, '\n\n\n')
 driver.implicitly_wait(3)
-msg_box = driver.find_elements_by_class_name('_2S1VP.copyable-text.selectable-text')[1]
+msg_box = driver.find_elements_by_class_name(
+    '_1awRl.copyable-text.selectable-text')[1]
 for i in range(1):
     print(i)
     msg_box.send_keys(msg)
